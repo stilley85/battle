6a63a30560918c9d10ac8a853ac1f537a24eb414
Stephen Tilley <stephentilley@gmail.com>
2018-02-06 15:32:10 +0000

    Use a POST redirect route to store session information

--- app.rb
+++ app.rb
@@ -2,13 +2,22 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     erb :index
   end
 
-  post '/play' do
-    @p1 = params[:p1]
-    @p2 = params[:p2]
+  post '/names' do
+    session[:p1] = params[:p1]
+    session[:p2] = params[:p2]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @p1 = session[:p1]
+    @p2 = session[:p2]
     erb :play
   end
 
